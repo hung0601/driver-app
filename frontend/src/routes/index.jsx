ddf875b306a00421aaafce9f3d2006c7c8dd2340
Đinh Thị Thu Hà <dinhthithuha1101@gmail.com>
2023-06-03 14:26:58 +0700

    popup cai dat lap lai

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../view/HomePage.jsx";
+import PopupRecurringSetting from "../component/popup_recurring_setting.jsx";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/popup",
+    element: <PopupRecurringSetting />,
   },
 ]);
