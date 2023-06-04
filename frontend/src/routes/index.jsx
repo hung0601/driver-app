52be3815fe82ff307bc6264fc8723bba2de4e78f
Đinh Thị Thu Hà <dinhthithuha1101@gmail.com>
2023-06-04 17:28:34 +0700

    create folder recurring

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../view/HomePage.jsx";
-import PopupRecurringSetting from "../component/popup_recurring_setting.jsx";
+import PopupRecurringSetting from "../component/popup_setting_recurring/popup_recurring_setting.jsx";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
   {
-    path: "/popup",
+    path: "/popuprecurring",
     element: <PopupRecurringSetting />,
   },
 ]);
